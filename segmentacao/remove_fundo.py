e8e0f51c8a079e2867016bf5efb814ff8369efc6
CiceroOliver <cicerojr2003@gmail.com>
2025-03-02 18:41:40 -0300

    feat: retorno da contornos de interesse em vermelho

--- segmentacao/remove_fundo.py
+++ segmentacao/remove_fundo.py
@@ -1,32 +1,27 @@
 import cv2
 import numpy as np
 
-def remove_fundo(mascara: np.ndarray, area_maxima: int = 40000) -> tuple:
+def remove_fundo(mascara: np.ndarray, area_maxima: int = 40000) -> np.ndarray:
     """
     Mantém apenas contornos fechados cujas áreas não excedem a área máxima especificada e que não tocam a borda da imagem.
 
     Parâmetros:
         mascara (np.ndarray): Máscara binária com os contornos.
-        area_maxima (int): Área máxima permitida para os contornos (default: 40000).
+        area_maxima (int): Área máxima permitida para os contornos (default: 40000) passível a validação.
 
     Retorna:
-        tuple:
-            - np.ndarray: Imagem com os contornos preenchidos em branco e o fundo preto.
-            - np.ndarray: Imagem de depuração com contornos originais em vermelho e novos contornos preenchidos em azul.
+        np.ndarray: Imagem com os novos contornos preenchidos em vermelho.
     """
     # Encontrar contornos na máscara
     contornos, _ = cv2.findContours(mascara, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-    # Criar uma máscara para preencher os contornos
-    mascara_preenchida = np.zeros_like(mascara)
-
-    # Criar uma imagem de depuração (em cores)
-    pulmao_contornado = cv2.cvtColor(mascara, cv2.COLOR_GRAY2BGR)  # Converter para BGR para usar cores
+    # Criar uma imagem para os contornos vermelhos
+    pulmao_contornado = np.zeros((mascara.shape[0], mascara.shape[1], 3), dtype=np.uint8)  # Imagem em preto
 
     # Obter as dimensões da imagem
     altura, largura = mascara.shape
 
-    # Filtrar e preencher apenas os contornos fechados que não tocam a borda e têm áreas menores ou iguais à área máxima
+    # Filtrar e desenhar apenas os contornos fechados que não tocam a borda e têm áreas menores ou iguais à área máxima
     for contorno in contornos:
         # Verificar se o contorno é fechado
         if cv2.arcLength(contorno, True) > 0:  # Verifica se o contorno tem comprimento positivo
@@ -38,34 +33,11 @@ def remove_fundo(mascara: np.ndarray, area_maxima: int = 40000) -> tuple:
                     toca_borda = True
                     break
 
-            # Se o contorno não tocar a borda e tiver área menor ou igual à área máxima, preencher
+            # Se o contorno não tocar a borda e tiver área menor ou igual à área máxima, desenhar em vermelho
             if not toca_borda:
                 area = cv2.contourArea(contorno)
                 if area <= area_maxima:
-                    # Preencher o contorno na máscara preenchida
-                    cv2.drawContours(mascara_preenchida, [contorno], -1, 255, -1)
+                    # Desenhar o contorno válido em azul na imagem de contornos vermelhos
+                    cv2.drawContours(pulmao_contornado, [contorno], -1, (255, 0, 0), 2)  # vermelho
 
-
-    # Desenhar os novos contornos preenchidos em azul na imagem de depuração
-    for contorno in contornos:
-        if cv2.arcLength(contorno, True) > 0:  # Verifica se o contorno tem comprimento positivo
-            toca_borda = False
-            for ponto in contorno:
-                x, y = ponto[0]
-                if x == 0 or x == largura - 1 or y == 0 or y == altura - 1:
-                    toca_borda = True
-                    break
-
-            if not toca_borda:
-                area = cv2.contourArea(contorno)
-                if area <= area_maxima:
-                    # Desenhar o contorno válido em vermelho na imagem de depuração
-                    cv2.drawContours(pulmao_contornado, [contorno], -1, (0, 0, 255), 2)  # Vermelho
-                else:
-                    # Definir pixels do contorno como preto na imagem de depuração (área muito grande)
-                    cv2.drawContours(pulmao_contornado, [contorno], -1, (0, 0, 0), -1)  # Preto
-            else:
-                # Definir pixels do contorno como preto na imagem de depuração (toca a borda)
-                cv2.drawContours(pulmao_contornado, [contorno], -1, (0, 0, 0), -1)  # Preto
-
-    return mascara_preenchida, pulmao_contornado
+    return pulmao_contornado
